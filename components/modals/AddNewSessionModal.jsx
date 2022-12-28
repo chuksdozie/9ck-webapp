98839985a9c3e56ea40a6cc92b3d46de0191186a
chuks dozie <chuksdozie48@gmail.com>
2022-12-28 22:06:36 +0100

    add new ssession and student

--- components/modals/AddNewSessionModal.jsx
+++ components/modals/AddNewSessionModal.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { colors, fontSizes } from "../../constants";
 import { AiFillCloseCircle } from "react-icons/ai";
 import AuthInput from "../inputs/AuthInput";
 import AuthButton from "../buttons/AuthButton";
+import Dropdown from "../inputs/Dropdown";
+import { getCourses } from "../../hooks/course.hook";
+import { getCamps } from "../../hooks/camp.hook";
+import { getLocations } from "../../hooks/location.hook";
+import { toast } from "react-toastify";
+import API from "../../api/api";
 
 const Container = styled.div`
   background-color: ${colors.light};
@@ -39,15 +45,120 @@ const SubTitle = styled.h2`
   color: ${colors.gray5};
 `;
 
-const AddNewSessionModal = () => {
+const AddNewSessionModal = ({ id }) => {
+  const [details, setDetails] = useState({
+    course_id: "",
+    camp_id: "",
+    location_id: "",
+    mode: "",
+  });
+
+  const [courses, setCourses] = useState([]);
+  const [camps, setCamps] = useState([]);
+  const [locations, setLocations] = useState([]);
+  const [mode, setMode] = useState([
+    { value: "online", text: "Online" },
+    { value: "offline", text: "Offline" },
+  ]);
+
+  const handleAddSession = async () => {
+    if (
+      !details.course_id ||
+      !details.camp_id ||
+      !details.location_id ||
+      !details.mode
+    ) {
+      toast.error("Please fill in all fields");
+      return;
+    }
+    try {
+      const { data } = await API.post(`/session/${id}/create`, details);
+      console.log(data);
+      toast.success(data?.message);
+      setDetails({
+        course_id: "",
+        camp_id: "",
+        location_id: "",
+        mode: "",
+      });
+      return;
+    } catch (error) {
+      console.log(error);
+      toast.error(error?.response?.data?.message);
+      return;
+    }
+  };
+
+  const getAll = async () => {
+    const fetchedCourses = await getCourses();
+    const fetchedCamps = await getCamps();
+    const fetchedLocations = await getLocations();
+    console.log("COURSES:", fetchedCourses);
+    console.log("CAMPS:", fetchedCamps);
+    console.log("LOCATIONS:", fetchedLocations);
+    for (let i = 0; i < fetchedCourses.length; i++) {
+      fetchedCourses[i].text = fetchedCourses[i].course_code;
+      fetchedCourses[i].value = fetchedCourses[i].id;
+    }
+    for (let i = 0; i < fetchedCamps.length; i++) {
+      fetchedCamps[i].text = fetchedCamps[i].name;
+      fetchedCamps[i].value = fetchedCamps[i].id;
+    }
+    for (let i = 0; i < fetchedLocations.length; i++) {
+      fetchedLocations[i].text = fetchedLocations[i].name;
+      fetchedLocations[i].value = fetchedLocations[i].id;
+    }
+    setCourses(fetchedCourses);
+    setCamps(fetchedCamps);
+    setLocations(fetchedLocations);
+  };
+
+  useEffect(() => {
+    getAll();
+  }, []);
+
+  useEffect(() => {
+    console.log(details);
+  }, [details]);
+
   return (
     <Container>
       <Title>Register Session</Title>
-      <AuthInput label={"Course"} placeholder={"Ex. Port Harcourt"} />
-      <AuthInput label={"Camp"} placeholder={"Ex. Rivers"} />
-      <AuthInput label={"Location"} placeholder={"Ex. 11 Abiye Odili"} />
-      <AuthInput label={"Mode"} placeholder={"Ex. Port Harcourt"} />
-      <AuthButton />
+      <Title>{id}</Title>
+      <Dropdown
+        options={courses}
+        label={"Course"}
+        value={details.course_id}
+        defaultText={"Select a course"}
+        onChange={(e) => setDetails({ ...details, course_id: e.target.value })}
+      />
+      <Dropdown
+        options={camps}
+        label={"Camp"}
+        value={details.camp_id}
+        defaultText={"Select a camp"}
+        onChange={(e) => setDetails({ ...details, camp_id: e.target.value })}
+      />
+      <Dropdown
+        options={locations}
+        label={"Location"}
+        value={details.location_id}
+        defaultText={"Select a location"}
+        onChange={(e) =>
+          setDetails({ ...details, location_id: e.target.value })
+        }
+      />
+      <Dropdown
+        options={mode}
+        label={"Mode"}
+        value={details.mode}
+        defaultText={"Select a mode"}
+        onChange={(e) => setDetails({ ...details, mode: e.target.value })}
+      />
+      <AuthButton
+        label={"Register Session"}
+        onClick={() => handleAddSession()}
+      />
     </Container>
   );
 };
--- components/modals/AddNewStudentModal.jsx
+++ components/modals/AddNewStudentModal.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { colors, fontSizes } from "../../constants";
 import { AiFillCloseCircle } from "react-icons/ai";
 import AuthInput from "../inputs/AuthInput";
 import AuthButton from "../buttons/AuthButton";
+import API from "../../api/api";
+import Dropdown from "../inputs/Dropdown";
+import { toast } from "react-toastify";
 
 const Container = styled.div`
   background-color: ${colors.light};
@@ -42,15 +45,78 @@ const SubTitle = styled.h2`
   color: ${colors.gray5};
 `;
 
-const AddNewStudentModal = () => {
+const AddNewStudentModal = ({ id }) => {
+  const [details, setDetails] = useState({
+    first_name: "",
+    last_name: "",
+    date_of_birth: "",
+    gender: "",
+  });
+
+  const handleAddStudent = async () => {
+    if (
+      !details.first_name ||
+      !details.last_name ||
+      !details.date_of_birth ||
+      !details.gender
+    ) {
+      toast.error("Please fill in all fields");
+      return;
+    }
+    try {
+      const { data } = await API.post(`/student/${id}/create`, details);
+      console.log(data);
+      toast.success(data?.message);
+      setDetails({
+        first_name: "",
+        last_name: "",
+        date_of_birth: "",
+        gender: "",
+      });
+      return;
+    } catch (error) {
+      console.log(error);
+      toast.error(error?.response?.data?.message);
+      return;
+    }
+  };
   return (
     <Container>
       <Title>Add a new student</Title>
-      <AuthInput label={"Firstname"} placeholder={"Ex. John"} />
-      <AuthInput label={"Lastname"} placeholder={"Ex. Doe"} />
-      <AuthInput label={"Date of birth"} placeholder={"Ex. 25/08/1995"} />
-      <AuthInput label={"Gender"} placeholder={"Ex. Male"} />
-      <AuthButton />
+      <Title>{id}</Title>
+      <AuthInput
+        label={"Firstname"}
+        placeholder={"Ex. John"}
+        type={"text"}
+        value={details.first_name}
+        onChange={(e) => setDetails({ ...details, first_name: e.target.value })}
+      />
+      <AuthInput
+        label={"Lastname"}
+        placeholder={"Ex. Doe"}
+        type={"text"}
+        value={details.last_name}
+        onChange={(e) => setDetails({ ...details, last_name: e.target.value })}
+      />
+      <AuthInput
+        label={"Date of birth"}
+        placeholder={"Ex. 25/08/1995"}
+        type="date"
+        value={details.date_of_birth}
+        onChange={(e) =>
+          setDetails({ ...details, date_of_birth: e.target.value })
+        }
+      />
+      <Dropdown
+        options={[
+          { value: "male", text: "Male" },
+          { value: "female", text: "Female" },
+        ]}
+        label={"Gender"}
+        defaultText={"Select a gender"}
+        onChange={(e) => setDetails({ ...details, gender: e.target.value })}
+      />
+      <AuthButton label={"Add Student"} onClick={() => handleAddStudent()} />
     </Container>
   );
 };
