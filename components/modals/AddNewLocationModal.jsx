fff815d24270abed9818ecda3f0fbfb174a3d0c5
chuks dozie <chuksdozie48@gmail.com>
2022-12-28 22:06:11 +0100

    add new location

--- components/modals/AddNewLocationModal.jsx
+++ components/modals/AddNewLocationModal.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { colors, fontSizes } from "../../constants";
 import { AiFillCloseCircle } from "react-icons/ai";
 import AuthInput from "../inputs/AuthInput";
 import AuthButton from "../buttons/AuthButton";
+import { toast } from "react-toastify";
+import API from "../../api/api";
 
 const Container = styled.div`
   background-color: ${colors.light};
@@ -40,13 +42,63 @@ const SubTitle = styled.h2`
 `;
 
 const AddNewLocationModal = () => {
+  const [details, setDetails] = useState({
+    name: "",
+    city: "",
+    address: "",
+    state: "",
+  });
+
+  const handleAddLocation = async () => {
+    if (!details.name || !details.city || !details.address || !details.state) {
+      toast.error("Please fill in all fields");
+      return;
+    }
+    try {
+      const { data } = await API.post("/location/add", details);
+      console.log(data);
+      toast.success(data?.message);
+      setDetails({
+        name: "",
+        city: "",
+        address: "",
+        state: "",
+      });
+      return;
+    } catch (error) {
+      console.log(error);
+      toast.error(error?.response?.data?.message);
+      return;
+    }
+  };
   return (
     <Container>
       <Title>Add a new location</Title>
-      <AuthInput label={"Address"} placeholder={"Ex. 11 Abiye Odili"} />
-      <AuthInput label={"City"} placeholder={"Ex. Port Harcourt"} />
-      <AuthInput label={"State"} placeholder={"Ex. Rivers"} />
-      <AuthButton />
+      <AuthInput
+        label={"Name"}
+        placeholder={"Ex. PortHarcout Camp 1"}
+        value={details.name}
+        onChange={(e) => setDetails({ ...details, name: e.target.value })}
+      />
+      <AuthInput
+        label={"Address"}
+        placeholder={"Ex. 11 Abiye Odili"}
+        value={details.address}
+        onChange={(e) => setDetails({ ...details, address: e.target.value })}
+      />
+      <AuthInput
+        label={"City"}
+        placeholder={"Ex. Port Harcourt"}
+        value={details.city}
+        onChange={(e) => setDetails({ ...details, city: e.target.value })}
+      />
+      <AuthInput
+        label={"State"}
+        placeholder={"Ex. Rivers"}
+        value={details.state}
+        onChange={(e) => setDetails({ ...details, state: e.target.value })}
+      />
+      <AuthButton label={"Add Location"} onClick={() => handleAddLocation()} />
     </Container>
   );
 };
